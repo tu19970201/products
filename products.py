cb84f2a6e006802b5a816eba75067a971cdae828
Alan Tu <tu19970201@gmail.com>
2022-04-23 12:47:37 +0800

    add header

--- products.py
+++ products.py
@@ -5,6 +5,7 @@ while True:
         break
 
     price = input('請輸入價格：')
+    price = int(price)
     products.append([name, price])
 print(products)
 #輸入商品價格
@@ -13,7 +14,8 @@ for p in products:
     print(p[0], '的價格是', p[1])
 #for loop顯示輸入結果
 
-with open('products.csv', 'w') as f:
+with open('products.csv', 'w', encoding = 'utf-8') as f:
+    f.write('商品,價格\n')
     for p in products:
-        f.write(p[0] + ',' + p[1] + '\n')
+        f.write(p[0] + ',' + str(p[1]) + '\n')
 #寫入csv
